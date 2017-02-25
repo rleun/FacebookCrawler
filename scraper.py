4def16ffb8c5cbad6b26540973a2b3c73fe9bb5c
Richel Leung <rleun005@gmail.com>
2017-02-25 13:15:32 -0800

    fb crawler scrolls page to add to csv

--- scraper.py
+++ scraper.py
@@ -11,20 +11,34 @@ app_secret = "0a79efd41c75ea74cc2796adc9257921"
 
 access_token = app_id + "|" + app_secret
 
-page_id = 'nytimes'
+page_id = 'buzzfeed'
 
-def testFacebookPageData(page_id, access_token):
+def testFacebookPageData(page_id, access_token, num_statuses):
 
-    base = "https://graph.facebook.com/v2.8"
+    base = "https://graph.facebook.com/"
     node = "/" + page_id + "/feed"
-    parameters = "/?access_token=%s" % access_token
+    parameters = "/?fields=message,link,created_time,type,name,id,likes.limit(1).summary(true),comments.limit(1).summary(true),shares&limit=%s&access_token=%s" % (num_statuses, access_token)
     url = base + node + parameters
 
     #Retrieve data
     req = urllib2.Request(url)
-    response = urllib2.urlopen(req)
+
+    #Stalls if request error
+    success = False
+    while success is False:
+        try:
+            response = urllib2.urlopen(req)
+            if response.getcode() == 200:
+                success = True
+        except Exception, e:
+            print # coding=utf-8
+            time.sleep(5)
+            print "Error for URL %s: %s" % (url, datetime.datetime.now())
+
     data = json.loads(response.read())
 
-    print json.dumps(data, indent=4, sort_keys=True)
+    return data
 
-testFacebookPageData(page_id, access_token)
+test_status = testFacebookPageData(page_id, access_token, 1)["data"][0]
+#testFacebookPageData(page_id, access_token)
+print json.dumps(test_status, indent=4, sort_keys=True)
